b1724df3401bdaae6bce303e76c322b7af9ba305
trong2409 <nvtrong249@gmail.com>
2022-06-30 23:26:46 +0700

    Dung khung layout

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,9 @@
+import classNames from "classnames/bind";
+import styles from "./Sidebar.module.scss"
+
 function Sidebar() {
-    return <div>
+    const cx = classNames.bind(styles)
+return <div className={cx('wrapper')}>
         Sidebar
     </div>;
 }
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,12 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
 function Header() {
-    return <div>
-        Header
+    const cx = classNames.bind(styles);
+
+    return <div className={cx('wrapper')}>
+        <div  className={cx('inner')}>
+            
+        </div>
     </div>;
 }
 
